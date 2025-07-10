8e67dc1bf3d6889ccda7e861c023e5e007f2b6e6
shimyubin <tla1161@naver.com>
2025-07-10 14:09:38 +0900

    fix: 응답부분 status 제거

--- src/controllers/estimate.controller.ts
+++ src/controllers/estimate.controller.ts
@@ -3,19 +3,18 @@ import estimateService from "../services/estimate.service";
 
 // 작성 가능한 리뷰 목록
 async function getWritableEstimates(req: Request, res: Response, next: NextFunction) {
-    try {
-        const clientId = req.auth!.userId;
-        const estimates = await estimateService.getWritableEstimates(clientId);
-        res.status(200).json({
-            status: 200,
-            message: "작성 가능한 리뷰 견적 목록 조회 성공",
-            data: estimates,
-        });
-    } catch (error) {
-        next(error);
-    }
+  try {
+    const clientId = req.auth!.userId;
+    const estimates = await estimateService.getWritableEstimates(clientId);
+    res.status(200).json({
+      message: "작성 가능한 리뷰 견적 목록 조회 성공",
+      data: estimates,
+    });
+  } catch (error) {
+    next(error);
+  }
 }
 
 export default {
-    getWritableEstimates,
+  getWritableEstimates,
 };
