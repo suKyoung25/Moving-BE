5eb8e110576c4f4636f46eab585366f0454b58bc
Hayoung Oh <scx200800@gmail.com>
2025-07-10 15:20:34 +0900

    feat: 견적 요청 API 구현

--- src/controllers/estimate.controller.ts
+++ src/controllers/estimate.controller.ts
@@ -1,4 +1,5 @@
 import { NextFunction, Request, Response } from "express";
+import { CreateRequestDto } from "../dtos/estimate.dto";
 import estimateService from "../services/estimate.service";
 
 // 작성 가능한 리뷰 목록
@@ -15,6 +16,24 @@ async function getWritableEstimates(req: Request, res: Response, next: NextFunct
   }
 }
 
+// 견적 요청 생성
+async function createEstimateRequest(
+  req: Request<{}, {}, CreateRequestDto>,
+  res: Response,
+  next: NextFunction,
+) {
+  try {
+    const newRequest = await estimateService.createEstimateRequest({
+      request: req.body,
+      clientId: req.auth?.userId ?? "e93cebbf-dedb-4f9c-aecd-7152fb6ad997", // TODO: 인증 미들웨어 추가시 수정
+    });
+    res.status(201).json({ data: newRequest });
+  } catch (error) {
+    next(error);
+  }
+}
+
 export default {
   getWritableEstimates,
+  createEstimateRequest,
 };
--- src/services/estimate.service.ts
+++ src/services/estimate.service.ts
@@ -1,6 +1,7 @@
 import { Client } from "@prisma/client";
 import estimateRepository from "../repositories/estimate.repository";
 import { BadRequestError } from "../types/errors";
+import { CreateRequestDto } from "../dtos/estimate.dto";
 
 // 작성 가능한 리뷰 목록
 async function getWritableEstimates(clientId: Client["id"]) {
@@ -10,6 +11,18 @@ async function getWritableEstimates(clientId: Client["id"]) {
   return estimateRepository.findWritableEstimatesByClientId(clientId);
 }
 
+// 견적 요청 생성
+async function createEstimateRequest({
+  request,
+  clientId,
+}: {
+  request: CreateRequestDto;
+  clientId: string;
+}) {
+  return await estimateRepository.createRequest(request, clientId);
+}
+
 export default {
   getWritableEstimates,
+  createEstimateRequest,
 };
