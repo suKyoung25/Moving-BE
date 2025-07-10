d900fcfab7bb4b9eba1dcda98ddcf1f49b4d826e
shimyubin <tla1161@naver.com>
2025-07-10 13:35:26 +0900

    fix: review 및 estimate controller에 있는 req.auth 수정

--- src/dtos/review.dto.ts
+++ src/dtos/review.dto.ts
@@ -2,22 +2,22 @@ import z from "zod";
 
 // 리뷰 생성 DTO 및 zod 스키마
 export const CreateReviewSchema = z.object({
-  estimateId: z.string().uuid(),
-  rating: z.number().int().min(1).max(5),
-  content: z.string().min(1),
-  moverId: z.string().uuid(),
+    estimateId: z.string().uuid(),
+    rating: z.number().int().min(1).max(5),
+    content: z.string().min(10),
+    moverId: z.string().uuid(),
 });
 export type CreateReviewDto = z.infer<typeof CreateReviewSchema>;
 
 // 리뷰 수정 DTO 및 zod 스키마
 export const UpdateReviewschema = z.object({
-  rating: z.number().int().min(1).max(5).optional(),
-  content: z.string().min(1).optional(),
+    rating: z.number().int().min(1).max(5).optional(),
+    content: z.string().min(1).optional(),
 });
 export type UpdateReviewDto = z.infer<typeof UpdateReviewschema>;
 
 // 리뷰 ID 파라미터 DTO 및 zod 스키마
 export const ReviewIdParamsSchema = z.object({
-  reviewId: z.string().uuid(),
+    reviewId: z.string().uuid(),
 });
 export type ReviewIdParamsDto = z.infer<typeof ReviewIdParamsSchema>;
