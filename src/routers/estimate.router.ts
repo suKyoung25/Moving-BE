be9104af2312f4c234b63d1917739852aadc543f
shimyubin <tla1161@naver.com>
2025-07-11 11:16:50 +0900

    feat: 나의 리뷰 리스트 페이지네이션 추가 및 fakeAuth 삭제

--- src/routers/estimate.router.ts
+++ src/routers/estimate.router.ts
@@ -1,14 +1,9 @@
 import { Router } from "express";
 import estimateController from "../controllers/estimate.controller";
-import fakeAuth from "../middlewares/fakeAuth";
 
 const estimateRouter = Router();
 
 // 작성 가능한 리뷰 목록
-estimateRouter.get(
-  "/writable/me",
-  fakeAuth,
-  estimateController.getWritableEstimates
-);
+estimateRouter.get("/writable/me", estimateController.getWritableEstimates);
 
 export default estimateRouter;
