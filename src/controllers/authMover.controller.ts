17d24338849c58fa5fa887c4db4f524499b3ca65
suKyoung25 <s.kyoung5496@gmail.com>
2025-07-11 11:50:53 +0900

    중간저장:회원가입 로직 fix 중

--- src/controllers/authMover.controller.ts
+++ src/controllers/authMover.controller.ts
@@ -8,44 +8,54 @@
 
 import { NextFunction, Request, Response } from "express";
 import authService from "../services/authMover.service";
-import { MoverSigninDto, MoverSignupDto } from "../dtos/auth/authMover.dto";
+import { MoverSigninDto, MoverSignupDto, signUpMoverSchema } from "../dtos/auth/authMover.dto";
+import { BadRequestError } from "../types/errors";
 
 //기사님 회원가입
 export async function moverSignup(
-    req: Request<{}, {}, MoverSignupDto>,
-    res: Response,
-    next: NextFunction,
+  req: Request<{}, {}, MoverSignupDto>,
+  res: Response,
+  next: NextFunction,
 ) {
-    const { name, email, phone, password } = req.body;
+  //req.body 유효성 검사
+  const parsed = signUpMoverSchema.safeParse(req.body);
+  if (!parsed.success) {
+    const ErrorMessage = Object.values(parsed.error.format())
+      .map((field: any) => field._errors.join(", "))
+      .join("; ");
+    throw new BadRequestError(ErrorMessage); //zod 에러메세지 사용함
+  }
 
-    try {
-        const mover = await authService.createMover({
-            name,
-            email,
-            phone,
-            password,
-        });
-        res.status(201).json({ mover: mover });
-    } catch (error) {
-        next(error);
-    }
+  const { name, email, phone, password } = req.body;
+
+  try {
+    const mover = await authService.createMover({
+      name,
+      email,
+      phone,
+      password,
+    });
+    res.status(201).json({ mover: mover });
+  } catch (error) {
+    next(error);
+  }
 }
 
 //기사님 로그인
 export async function moverSignin(
-    req: Request<{}, {}, MoverSigninDto>,
-    res: Response,
-    next: NextFunction,
+  req: Request<{}, {}, MoverSigninDto>,
+  res: Response,
+  next: NextFunction,
 ) {
-    const { email, password } = req.body;
+  const { email, password } = req.body;
 
-    try {
-        const mover = await authService.getMoverByEmail({
-            email,
-            password,
-        });
-        res.status(201).json({ mover: mover });
-    } catch (error) {
-        next(error);
-    }
+  try {
+    const mover = await authService.getMoverByEmail({
+      email,
+      password,
+    });
+    res.status(201).json({ mover: mover });
+  } catch (error) {
+    next(error);
+  }
 }
--- src/dtos/auth/authMover.dto.ts
+++ src/dtos/auth/authMover.dto.ts
@@ -1,5 +1,36 @@
+import z from "zod";
 import { createMoverInput, getMoverInput } from "../../types";
+import { ErrorMessage } from "../../constants/ErrorMessage";
 
+//기사님 회원가입 DTO
 export interface MoverSignupDto extends createMoverInput {}
 
+//기사님 로그인 DTO
 export interface MoverSigninDto extends getMoverInput {}
+
+//기사님 회원가입 유효성 검사
+export const signUpMoverSchema = z
+  .object({
+    name: z.string().max(4, ErrorMessage.NAME_LENGTH_LIMIT).nonempty(ErrorMessage.NO_NAME),
+
+    email: z.string().email().nonempty(ErrorMessage.NO_EMAIL),
+
+    phoneNumber: z
+      .string()
+      .regex(/^\d{9,11}$/, ErrorMessage.PHONE_REGEX)
+      .nonempty(ErrorMessage.NO_PHONE),
+
+    password: z
+      .string()
+      .min(8, ErrorMessage.PASSWORD_LENGTH_LIMIT)
+      .regex(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/, ErrorMessage.PASSWORD_REGEX)
+      .nonempty(ErrorMessage.NO_PASSWORD),
+
+    passwordConfirmation: z.string(),
+  })
+  .refine((data) => data.password === data.passwordConfirmation, {
+    message: ErrorMessage.PASSWORD_CONFIRMATION_NOT_MATCH,
+    path: ["passwordConfirmation"],
+  });
+
+// export type SignUpMoverData = z.infer<typeof signUpMoverSchema>;
