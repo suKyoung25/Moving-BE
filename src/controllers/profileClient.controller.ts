5bec1ac4ff1f60b4b37fe7d6360b80ff6d345a56
sky <blooming_garden_@naver.com>
2025-07-15 17:08:04 +0900

    feat: 프로필 등록 api 완성

--- src/controllers/profileClient.controller.ts
+++ src/controllers/profileClient.controller.ts
@@ -1,16 +1,31 @@
 import { Request, Response, NextFunction } from "express";
-import { ProfilePostRequest } from "../dtos/profileClient.dto";
+import { clientProfileSchema, ProfilePostDto } from "../dtos/profileClient.dto";
+import profileClientService from "../services/profileClient.service";
+import { MoveType } from "@prisma/client";
 
-// async function post(req: Request<{}, {}, ProfilePostRequest>, res: Response, next: NextFunction) {
-//   try {
-//     const parsedData = profileCl
-//   } catch (error) {
-//     next(error)
-//   }
-// }
+async function post(req: Request<{}, {}, ProfilePostDto>, res: Response, next: NextFunction) {
+  try {
+    const { userId } = req.auth!;
+
+    // living area 빈 배열 undefined 타입 오류 방지
+    const parsedData = clientProfileSchema.parse(req.body);
+
+    const profile = {
+      profileImage: parsedData.profileImage,
+      serviceType: parsedData.serviceType as MoveType[], // 타입 캐스팅
+      livingArea: parsedData.livingArea,
+    };
+
+    await profileClientService.create(userId, profile);
+
+    res.status(201).json({ message: "일반 프로필 등록 성공", data: parsedData });
+  } catch (error) {
+    next(error);
+  }
+}
 
 const profileClientController = {
-  // post,
+  post,
 };
 
 export default profileClientController;
