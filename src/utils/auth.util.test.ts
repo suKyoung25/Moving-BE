c6dd5b87bdfe60468dca1f51f2b162e06d5a22dc
sky <blooming_garden_@naver.com>
2025-08-03 20:42:03 +0900

    test: authUtil 검사 끝

--- src/utils/auth.util.test.ts
+++ src/utils/auth.util.test.ts
@@ -22,7 +22,7 @@ describe("hashPassword 검사", () => {
   });
 });
 
-describe.only("filterSensitiveUserData 검사", () => {
+describe("filterSensitiveUserData 검사", () => {
   // 1. 사전 준비
   beforeEach(() => {
     jest.clearAllMocks();
@@ -41,5 +41,55 @@ describe.only("filterSensitiveUserData 검사", () => {
       name: mock.fullClientInfo.name,
       phone: mock.fullClientInfo.phone,
     });
+  });
+
+  test("소셜 로그인 시, 비밀번호와 providerId를 제거해 반환한다.", () => {
+    // 2. 실행
+    const result = authUtil.filterSensitiveUserData(mock.fullClientSocialInfoAfter as any);
+
+    // 3. 검증
+    expect(result).not.toHaveProperty("hashedPassword");
+    expect(result).not.toHaveProperty("providerId");
+    expect(result).toMatchObject({
+      id: mock.fullClientSocialInfoAfter.id,
+      email: mock.fullClientSocialInfoAfter.email,
+      name: mock.fullClientSocialInfoAfter.name,
+      phone: mock.fullClientSocialInfoAfter.phone,
+    });
+  });
+});
+
+describe("verifyPassword 검사", () => {
+  // 0. 준비
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test("비밀번호가 일치하면 true를 반환한다.", async () => {
+    // 1. 준비
+    const inputPassword = "asdf1234";
+    const savedPassword = "asdf1234";
+
+    (bcrypt.compare as jest.Mock).mockResolvedValue(true);
+
+    // 2. 실행
+    const result = await authUtil.verifyPassword(inputPassword, savedPassword);
+
+    // 3. 검증
+    expect(result).toBeTruthy();
+    expect(bcrypt.compare).toHaveBeenCalledWith(inputPassword, savedPassword);
+  });
+
+  test("비밀번호가 일치하지 않으면 오류를 던진다.", async () => {
+    // 1. 준비
+    const inputPassword = "asdf1234";
+    const savedPassword = "asdf1234!";
+
+    (bcrypt.compare as jest.Mock).mockResolvedValue(false);
+
+    // 2-3. 실행 및 검증 -- 오류 나면 끝까지 도달을 못 해서 바로 rejects 던져야 함
+    await expect(authUtil.verifyPassword(inputPassword, savedPassword)).rejects.toThrow(
+      ConflictError,
+    );
   });
 });
