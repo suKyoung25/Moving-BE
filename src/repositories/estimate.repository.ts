71c0b3947108675fd750b621a7f2294c67d5cccd
Hayoung Oh <scx200800@gmail.com>
2025-08-04 14:06:49 +0900

    fix: Request 스키마 estimate, designatedRequest 복수로 수정

--- src/repositories/estimate.repository.ts
+++ src/repositories/estimate.repository.ts
@@ -24,7 +24,7 @@ async function findWritableEstimatesByClientId(
           select: {
             moveType: true,
             moveDate: true,
-            designatedRequest: {
+            designatedRequests: {
               select: { moverId: true },
             },
           },
@@ -98,7 +98,7 @@ async function findPendingEstimatesByClientId(clientId: Client["id"], offset = 0
           fromAddress: true,
           toAddress: true,
           moveType: true,
-          designatedRequest: {
+          designatedRequests: {
             select: {
               moverId: true,
             },
@@ -160,7 +160,7 @@ async function findReceivedEstimatesByClientId(clientId: string, page: number, l
         mover: true,
         request: {
           include: {
-            designatedRequest: true,
+            designatedRequests: true,
           },
         },
       },
@@ -264,7 +264,7 @@ async function findEstimateDetailById(estimateId: string, clientId: string) {
             fromAddress: true,
             toAddress: true,
             requestedAt: true,
-            designatedRequest: true,
+            designatedRequests: true,
           },
         },
         mover: {
@@ -334,7 +334,7 @@ async function getRejectedEstimates(moverId: string, page: number) {
             client: {
               select: { name: true },
             },
-            designatedRequest: {
+            designatedRequests: {
               select: { moverId: true },
             },
           },
@@ -387,7 +387,7 @@ async function getPaginatedSentEstimates(moverId: string, page: number) {
             client: {
               select: { name: true },
             },
-            designatedRequest: {
+            designatedRequests: {
               select: { moverId: true },
             },
           },
