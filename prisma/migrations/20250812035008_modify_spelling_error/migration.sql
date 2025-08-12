f5edd5179ccb16089cc848ce0c69666a9f6f7cee
Hayoung Oh <scx200800@gmail.com>
2025-08-12 12:50:50 +0900

    fix: CANCELED 맞춤법 수정

---
+++ prisma/migrations/20250812035008_modify_spelling_error/migration.sql
@@ -0,0 +1,14 @@
+/*
+  Warnings:
+
+  - The values [CANCELD] on the enum `NotificationType` will be removed. If these variants are still used in the database, this will fail.
+
+*/
+-- AlterEnum
+BEGIN;
+CREATE TYPE "NotificationType_new" AS ENUM ('NEW_ESTIMATE', 'ESTIMATE_CONFIRMED', 'ESTIMATE_REJECTED', 'CANCELED', 'MOVING_DAY');
+ALTER TABLE "Notification" ALTER COLUMN "type" TYPE "NotificationType_new" USING ("type"::text::"NotificationType_new");
+ALTER TYPE "NotificationType" RENAME TO "NotificationType_old";
+ALTER TYPE "NotificationType_new" RENAME TO "NotificationType";
+DROP TYPE "NotificationType_old";
+COMMIT;
