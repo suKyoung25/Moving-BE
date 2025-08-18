055d600c48d97460fdbba6649fabb4db21652555
sumin <shinmilli100@gmail.com>
2025-08-18 15:58:23 +0900

    refactor: 리뷰 router 수정

--- src/routers/review.router.ts
+++ src/routers/review.router.ts
@@ -5,65 +5,34 @@ import { Router } from "express";
 
 const reviewRouter = Router();
 
+// 내가 작성한 리뷰 목록
 reviewRouter.get("/me", verifyAccessToken, reviewController.getReviews);
+
+// 작성 가능한 리뷰 목록
 reviewRouter.get("/writable", verifyAccessToken, reviewController.getWritableReviews);
+
+// 기사님 본인 리뷰 목록
 reviewRouter.get("/mover", verifyAccessToken, reviewController.getMoverOwnReviews);
+
+// 특정 기사님에게 달린 리뷰 목록 (공개용)
 reviewRouter.get("/mover/:moverId", reviewController.getReviews); // 이것을 마지막에
 
+// 리뷰 작성
 reviewRouter.post(
   "/",
   verifyAccessToken,
   validateReq(CreateReviewSchema),
   reviewController.createReview,
 );
+
+// 리뷰 수정
 reviewRouter.patch(
   "/:reviewId",
   verifyAccessToken,
   validateReq(UpdateReviewschema),
   reviewController.updateReview,
 );
+// 리뷰 삭제
 reviewRouter.delete("/:reviewId", verifyAccessToken, reviewController.deleteReview);
-
-// // 내가 작성한 리뷰 목록
-// reviewRouter.get(
-//   "/me",
-//   verifyAccessToken,
-//   reviewController.getReviews,
-// );
-
-// // 기사님 본인 리뷰 목록
-// reviewRouter.get(
-//   "/mover",
-//   verifyAccessToken,
-//   reviewController.getMoverOwnReviews,
-// );
-
-// // 특정 기사님에게 달린 리뷰 목록 (공개용)
-// reviewRouter.get(
-//   "/mover/:moverId",
-//   reviewController.getReviews,
-// );
-
-// // 리뷰 작성
-// reviewRouter.post(
-//   "/",
-//   verifyAccessToken,
-//   validateReq(CreateReviewSchema),
-//   reviewController.createReview,
-// );
-
-// // 리뷰 수정
-// reviewRouter.patch(
-//   "/:reviewId",
-//   verifyAccessToken,
-//   validateReq(UpdateReviewschema),
-//   reviewController.updateReview,
-// );
-
-// // 리뷰 삭제
-// reviewRouter.delete("/:reviewId", verifyAccessToken, reviewController.deleteReview);
-
-// // 작성 가능한 리뷰 목록
-// reviewRouter.get("/writable", verifyAccessToken, reviewController.getWritableReviews);
 
 export default reviewRouter;
